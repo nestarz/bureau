cb8566a7dc7fab8ec88ccffa30cfc9c9268e349b
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-18 17:56:09 +0200

    fix: client.ts

--- src/components/Client.tsx
+++ src/components/Client.tsx
@@ -3,7 +3,7 @@ import {
   useEffect,
   useState,
 } from "https://esm.sh/preact@10.15.1/hooks.js?target=es2022";
-import island from "https://deno.land/x/islet@0.0.8/island.ts";
+import island from "https://deno.land/x/islet@0.0.8/client.ts";
 export { h, hydrate } from "https://esm.sh/preact@10.15.1?target=es2022";
 
 const Home = globalThis.window?.document
