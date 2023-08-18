d4a7a0950d314db027c1e3c6fae392789fc50a08
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-18 17:53:22 +0200

    fix: use islet

--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -1,11 +1,15 @@
-import { jsx as jsx_ } from "preact/jsx-runtime";
-import { toChildArray, h, cloneElement } from "preact";
-import { createJsx } from "islands/mod.ts";
+export * from "preact/jsx-runtime";
+import { jsx as _jsx } from "preact/jsx-runtime";
+import { cloneElement, toChildArray, Fragment } from "preact";
+import { createJsx } from "https://deno.land/x/islet@0.0.8/server.ts";
+
 export const jsx = createJsx({
-  jsx: jsx_,
-  h,
-  toChildArray,
+  jsx: _jsx,
   cloneElement,
+  h: _jsx,
+  toChildArray,
+  Fragment,
   key: "cms",
   prefix: "/admin",
 });
+export const jsxs = jsx;
